1404f575dc72cfd8f3447d863351c0327c4dd44d
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-10-16 11:26:37 -0300

    M9 L6: Changing Informatioins of Reducer in Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import {useAppSelector} from './redux/hooks/useAppSelector';
+import React from 'react';
+import { useAppSelector } from './redux/hooks/useAppSelector';
+import { setName, setAge } from './redux/reducers/userReducer'
+import { useDispatch } from 'react-redux';
 
 function App() {
   const user = useAppSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  const handleNameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setName(event.target.value));
+  }
+  const handleAgeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setAge(event.target.value));
+  }
 
   return (
     <div>
@@ -12,7 +23,8 @@ function App() {
       Tema: ...
 
       <hr />
-      <input type="text" value={'...'} />
+      <input type="text" value={user.name} onChange={handleNameInput}/>
+      <input type="text" value={user.age} onChange={handleAgeInput}/>
 
       <hr />
       <button>Switch Theme</button>
